e4344bfe22ceb029a2d7bf57530ab661d72ec081
Felipe Lube de Bragança <felipe.lubra@gmail.com>
2018-12-06 18:55:16 -0200

    refatoração: mais robustez e definção de status intermediários

--- xram_memory/news_fetcher/fetcher.py
+++ xram_memory/news_fetcher/fetcher.py
@@ -18,6 +18,10 @@ saved_pages_dir = settings.NEWS_FETCHER_STATIC_DIR
 @job
 def process_news(archived_news):
     try:
+
+        archived_news.status = ArchivedNews.STATUS_QUEUED_BASIC_INFO
+        archived_news.save()
+
         article = Article(archived_news.url)
         article.download()
         article.parse()
@@ -29,29 +33,42 @@ def process_news(archived_news):
         archived_news.images = ",".join(article.images)
         archived_news.text = article.text
         archived_news.summary = article.summary
-        archived_news.status = ArchivedNews.STATUS_PROCESSED
+
+    except Exception as err:
+
+        try:
+            archived_news.status = ArchivedNews.STATUS_ERROR_NO_PROCESS
+            archived_news.save()
+        finally:
+            logger.error(
+                'Falha ao processar Notícia com o id {} e status "{}": {}'.format(
+                    archived_news.id, archived_news.get_status_display(), str(err)
+                )
+            )
+
+    else:
+
+        status_before = archived_news.get_status_display()
+        archived_news.status = ArchivedNews.STATUS_PROCESSED_BASIC_INFO
         archived_news.save()
         logger.info(
             'Sucesso ao processar Notícia com o id {} e status "{}".'.format(
-                archived_news.id, archived_news.get_status_display()
+                archived_news.id, status_before
             )
         )
-    except:
-        logger.error(
-            'Falha ao processar Notícia com o id {} e status "{}".'.format(
-                archived_news.id, archived_news.get_status_display()
-            )
-        )
-        try:
-            archived_news.status = ArchivedNews.STATUS_ERROR
-            archived_news.save()
-        except:
-            pass
 
 
 @job
 def save_news_as_pdf(archived_news):
     try:
+
+        if not saved_pages_dir:
+            raise ValueError(
+                'O caminho para onde salvar as páginas não foi definido (constante de configuração NEWS_FETCHER_STATIC_DIR).')
+
+        archived_news.status = ArchivedNews.STATUS_QUEUED_PAGE_CAPTURE
+        archived_news.save()
+
         uniq_filename = str(datetime.datetime.now().date(
         )) + '_' + str(datetime.datetime.now().time()).replace(':', '.') + '.pdf'
 
@@ -62,23 +79,28 @@ def save_news_as_pdf(archived_news):
         tic = default_timer()
         pdfkit.from_url(archived_news.url, archived_news_pdf_path, options={
             'print-media-type': None,
-            'disable-javascript': None
+            'disable-javascript': None,
         })
         toc = default_timer()
 
+    except Exception as err:
+
+        try:
+            archived_news.status = ArchivedNews.STATUS_ERROR_NO_CAPTURE
+            archived_news.save()
+        finally:
+            logger.error(
+                'Falha ao tentar salvar a Notícia em formato PDF com o id {} e status "{}".'.format(
+                    archived_news.id, archived_news.get_status_display()
+                )
+            )
+
+    else:
+
+        archived_news.status = ArchivedNews.STATUS_PROCESSED_PAGE_CAPTURE
+        archived_news.save()
         logger.info(
             'Notícia com o id {} salva em formato PDF "{}" em {}s.'.format(
                 archived_news.id, archived_news_pdf_path, toc - tic
             )
         )
-    except:
-        logger.error(
-            'Falha ao tentar salvar a Notícia em formato PDF com o id {} e status "{}".'.format(
-                archived_news.id, archived_news.get_status_display()
-            )
-        )
-        try:
-            archived_news.status = ArchivedNews.STATUS_ERROR_NO_CAPTURE
-            archived_news.save()
-        except:
-            pass
