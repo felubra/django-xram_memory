1b360d8891bfd446b237a47a56cc503db665cf69
Felipe Lube de Bragança <felipe.lubra@gmail.com>
2018-12-12 00:26:17 -0200

    se não houver conexão disponivel com o redis, altere o status da notícia

--- xram_memory/news_fetcher/signals.py
+++ xram_memory/news_fetcher/signals.py
@@ -4,6 +4,7 @@ from ..archived_news.models import ArchivedNews
 from .fetcher import process_news, save_news_as_pdf, verify_if_in_archive_org
 import logging
 import django_rq
+import redis
 
 
 logger = logging.getLogger(__name__)
@@ -21,9 +22,6 @@ def add_news_archive_to_queue(sender, **kwargs):
 
         if archived_news.needs_reprocessing:
             try:
-                # @todo verificar se o serviço de filas está funcionando (se existe conexão com o redis)
-                # e logar um aviso caso contrário
-
                 # Adicione uma flag ao objeto para evitar que esse handler execute infinitamente,
                 # já que algumas das funções abaixo podem chamar o save()
                 archived_news._job_processing = True
@@ -36,9 +34,20 @@ def add_news_archive_to_queue(sender, **kwargs):
 
                 if archived_news.force_pdf_capture:
                     save_news_as_pdf.delay(archived_news)
-                # adicione a notícia na fila para  baixar
-                # altere o status para 'agendado'
-                # salve o modelo
+
+                # @todo configurar o django_rq para reutilizar a conexão do cache do redis-cache
+                # verifique se o serviço de filas está funcionando (se existe conexão com o redis)
+                fq = django_rq.queues.get_failed_queue()
+                if fq.count > 0:
+                    logger.warn(
+                        "Existem {} trabalhos que falharam na fila.".format(fq.count))
+            except redis.exceptions.ConnectionError as e:
+                logger.error(
+                    "Não foi possível processar os trabalhos da fila para a notícia {}, porque o servidor do Redis não está disponível no momento: {}".format(
+                        archived_news.pk, e)
+                )
+                archived_news.status = ArchivedNews.STATUS_ERROR_NO_QUEUE
+                archived_news.save()
             finally:
                 del archived_news._job_processing
     except:
