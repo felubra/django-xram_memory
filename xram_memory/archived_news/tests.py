94245695adf6bc10f362398a5e68d45ae662d33c
Felipe Lube de Bragança <felipe.lubra@gmail.com>
2018-12-12 18:58:47 -0200

    Testes com o formulário administrativo para ArchivedNews - Verifique que o nosso campo virtual 'insertion_mode' está no form - [WIP] teste a presença de campos quando editando

--- xram_memory/archived_news/tests.py
+++ xram_memory/archived_news/tests.py
@@ -1,8 +1,26 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 from .models import ArchivedNews, Keyword
 from django.template.defaultfilters import slugify
+from django.contrib.admin.sites import AdminSite
+from django.contrib.admin.options import ModelAdmin
+from .admin import ArchivedNewsAdmin
+from ..users.models import User
+from django.urls import reverse
+
 
 # Create your tests here.
+
+class MockRequest:
+    pass
+
+
+class MockSuperUser:
+    def has_perm(self, perm):
+        return True
+
+
+request = MockRequest()
+request.user = MockSuperUser()
 
 
 class ArchivedNewsTestCase(TestCase):
@@ -30,3 +48,40 @@ class KeywordTestCase(TestCase):
     def test_keyword_slug(self):
         '''Teste que a slug está sendo criada depois de o modelo ser salvo'''
         self.assertEqual(self.keyword.slug, slugify("Abacate é uma fruta"))
+
+
+class ArchivedNewsAdminFormTestCase(TestCase):
+    def setUp(self):
+        self.automatic_archived_news = ArchivedNews(
+            url="https://politica.estadao.com.br/noticias/geral,em-diplomacao-bolsonaro-diz-que-a-soberania-do-voto-popular-e-inquebrantavel,70002640605")
+        self.site = AdminSite()
+        self.user_info = {
+            'username': 'admin',
+            'email': 'test@test.com',
+            'password': 'test@test.com'
+        }
+        self.user = User.objects.create_superuser(**self.user_info)
+        self.client = Client()
+
+    def test_fields_for_new_item(self):
+        '''Testa a presença/ausência e os valores dos campos quando em modo de inserção'''
+        archived_news_admin = ArchivedNewsAdmin(ArchivedNews, self.site)
+        base_fields = archived_news_admin.get_form(request).base_fields
+
+        # Verifica que o nosso campo 'virtual' 'insertion_mode' está presente no formulário
+        self.assertIn('insertion_mode', base_fields)
+
+    # @todo
+    def test_fields_for_existing_item(self):
+        '''Testa a presença/ausência e os valores dos campos quando em modo de edição'''
+        self.automatic_archived_news.save()
+        #archived_news_admin = ArchivedNewsAdmin(ArchivedNews, self.site)
+        # Crie um cliente para entrar na interface administrativa
+        c = Client()
+        # Faça login com o nosso usuário super administrador criado
+        c.login(**self.user_info)
+        archived_news_change_url = reverse(
+            "admin:archived_news_archivednews_change", args=[self.automatic_archived_news.pk])
+        response = c.get(archived_news_change_url)
+        admin_form = response.context_data['adminform']
+        pass
