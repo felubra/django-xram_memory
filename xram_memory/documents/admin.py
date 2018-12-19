08ed0c53b0c5fdee18447c6892d6680cf2172857
Felipe Lube de Bragança <felipe.lubra@gmail.com>
2018-12-19 11:54:15 -0200

    interface adminstrativa básica para os modelos do tipo documento

--- xram_memory/documents/admin.py
+++ xram_memory/documents/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import ArchivedNewsPDFCapture
+
+
+@admin.register(ArchivedNewsPDFCapture)
+class ArchivedNewsPDFCaptureAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'created_by',
+        'modified_by',
+        'created_at',
+        'modified_at',
+        'size',
+        'mime_type',
+        'url_of_capture',
+        'pdf_file',
+        'archived_news',
+    )
+    list_filter = (
+        'created_by',
+        'modified_by',
+        'created_at',
+        'modified_at',
+        'archived_news',
+    )
+    date_hierarchy = 'created_at'
