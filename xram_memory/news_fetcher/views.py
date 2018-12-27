b444155134bbb3c47b3f6bbcc7d4f95917fa8f09
Felipe Lube de Bragança <felipe.lubra@gmail.com>
2018-12-27 18:58:02 -0200

    implementação inicial da view para pegar informações sobre uma notícia

--- xram_memory/news_fetcher/views.py
+++ xram_memory/news_fetcher/views.py
@@ -1,3 +1,28 @@
+from django import forms
+
+
 from django.shortcuts import render
+from .fetcher import extract_basic_info_from_url
+from django.http import HttpResponseNotAllowed, HttpResponseBadRequest, JsonResponse
+from django.contrib.admin.views.decorators import staff_member_required
+
 
 # Create your views here.
+
+class URLForm(forms.Form):
+    url = forms.fields.URLField()
+
+
+@staff_member_required
+def return_basic_info(request):
+    # if request.isAjax()
+    if request.method == 'POST':
+        form = URLForm(request.POST)
+        if form.is_valid():
+            url = request.POST['url']
+            basic_info = extract_basic_info_from_url(url)
+            return JsonResponse(basic_info)
+        else:
+            return HttpResponseBadRequest('Bad request')
+    else:
+        return HttpResponseNotAllowed(['POST'])
