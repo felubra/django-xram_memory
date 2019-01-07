49a5958ddcea5e15e2341714dff4cb901a4ecc8b
Felipe Lube de Bragança <felipe.lubra@gmail.com>
2019-01-07 16:25:05 -0200

    App para coleções de artefatos

---
+++ xram_memory/collection/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+
+from .models import Collection
+
+
+@admin.register(Collection)
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'created_by',
+        'modified_by',
+        'created_at',
+        'modified_at',
+        'text',
+    )
+    list_filter = ('created_by', 'modified_by', 'created_at', 'modified_at')
+    raw_id_fields = ('items',)
+    date_hierarchy = 'created_at'
