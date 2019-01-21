655def3a829bad40d3d5648110a7e556addd0d1d
Felipe Lube de Bragança <felipe.lubra@gmail.com>
2019-01-21 12:18:17 -0200

    feat(doc): documentação dos recebedores de sinal

--- xram_memory/artifact/receivers.py
+++ xram_memory/artifact/receivers.py
@@ -5,17 +5,22 @@ from xram_memory.artifact.models import Document, ImageDocument, PDFDocument
 
 
 @receiver(post_save)
-def set_mimetype_filesize(sender, **kwargs):
+def set_mimetype_filesize_for_documents(sender, **kwargs):
+    """
+    Defina o tamanho e o tipo do arquivo para documentos.
+    """
     instance = kwargs['instance']
 
     if hasattr(instance, '_save_in_signal'):
         return
 
-    if isinstance(instance, (ImageDocument, PDFDocument,)):
+    if isinstance(instance, (Document)):
         try:
-            # TODO: otimizar com lru_cache utilizando o hash do arquivo
+            # TODO: otimizar as funções abaixo com lru_cache utilizando o hash do arquivo
             instance.determine_mime_type()
             instance.determine_file_size()
+            # Adicione uma flag privada no modelo para evitar que esse handler execute
+            # infinitamente, já que vamos chamar o save() aqui
             instance._save_in_signal = True
             instance.save()
         finally:
