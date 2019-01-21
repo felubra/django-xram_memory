189970f3640c1b74ead175f79e11109b2e72106f
Felipe Lube de Bragança <felipe.lubra@gmail.com>
2019-01-21 18:53:19 -0200

    feat: altere a linguagem do django para pt-br

--- xram_memory/settings.py
+++ xram_memory/settings.py
@@ -103,7 +103,7 @@ class Common(Configuration):
 
     # Internationalization
     # https://docs.djangoproject.com/en/2.1/topics/i18n/
-    LANGUAGE_CODE = 'en-us'
+    LANGUAGE_CODE = 'pt-br'
 
     TIME_ZONE = 'America/Sao_Paulo'
 
