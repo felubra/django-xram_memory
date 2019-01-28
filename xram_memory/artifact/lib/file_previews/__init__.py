58d1b5f2a40dc7cc691d0226398a5d0b2e3cf4bb
Felipe Lube de Bragança <felipe.lubra@gmail.com>
2019-01-28 00:17:35 -0200

    feat: melhor geração de visualizações pdf
    - gere a visualizações num diretório temporário
    - use apenas o arquivo da primeira página como fonte para a miniatura
    - apague arquivos temporários gerados

--- xram_memory/artifact/lib/file_previews/__init__.py
+++ xram_memory/artifact/lib/file_previews/__init__.py
@@ -1,3 +1,5 @@
+import os
+from django.utils.six import BytesIO
 from tempfile import gettempdir
 from pdf2image import convert_from_path, convert_from_bytes
 try:
@@ -9,6 +11,31 @@ except ImportError:
 def pdf_preview(source, exif_orientation=True, **options):
     if not source:
         return
+
+    # Coverta a primeira página do arquivo pdf e salve-a como arquivo temporário
     images = convert_from_path(
-        source.path, first_page=0, last_page=1)
-    return images[0]
+        source.path, first_page=0, last_page=1, output_folder=gettempdir())
+
+    # Não estou certo se images vai ter sempre apenas um arquivo gerado, então vamos verificar todos
+    for index, image in enumerate(images):
+        # só nos interessa usar a primeira página
+        if index == 0:
+            with open(image.filename, mode="rb") as f:
+                source = BytesIO(f.read())
+        # feche o arquivo temporário
+        image.fp.close()
+        # apague o arquivo temporário
+        os.remove(image.filename)
+
+    # Código de https://github.com/SmileyChris/easy-thumbnails/blob/master/easy_thumbnails/source_generators.py
+    image = Image.open(source)
+    try:
+        # An "Image file truncated" exception can occur for some images that
+        # are still mostly valid -- we'll swallow the exception.
+        image.load()
+    except IOError:
+        pass
+    # Try a second time to catch any other potential exceptions.
+    image.load()
+
+    return image
