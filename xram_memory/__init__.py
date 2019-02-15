bd74b0f29e0ab11cb4d48d861a1e8e9f3f6543b4
Felipe Lube de Bragança <felipe.lubra@gmail.com>
2019-02-15 00:22:13 -0200

    feat: é possível agendar tarefas com o celery antes de iniciar o app?

--- xram_memory/__init__.py
+++ xram_memory/__init__.py
@@ -1,7 +1,43 @@
 from __future__ import absolute_import, unicode_literals
-
+import socket
+from django.core.checks import Error, register
+from kombu import Connection
+from kombu.exceptions import OperationalError
+from django.conf import settings
 # This will make sure the app is always imported when
 # Django starts so that shared_task will use this app.
 from .celery import app as celery_app
+
+
+@register()
+def celery_broker_check(app_configs, **kwargs):
+    errors = []
+    try:
+        conn = Connection(settings.CELERY_BROKER_URL)
+        conn.ensure_connection(max_retries=3)
+        from celery.task.control import inspect
+        insp = inspect()
+        d = insp.stats()
+        if not d:
+            errors.append(
+                Error(
+                    'Nenhum worker do Celery foi encontrado',
+                    hint="Tenha certeza de que você iniciou ao menos um worker",
+                    obj=inspect,
+                    id='xram_memory.CeleryNoWorkerError',
+                )
+            )
+    except (IOError, OperationalError):
+        errors.append(
+            Error(
+                'Falha ao tentar conexão com o message broker do Celery',
+                hint='Verifique se o broker está rodando e disponível em {}.'.format(
+                    settings.CELERY_BROKER_URL),
+                obj=conn,
+                id='xram_memory.CeleryBrokerConnectionError',
+            )
+        )
+    return errors
+
 
 __all__ = ('celery_app',)
