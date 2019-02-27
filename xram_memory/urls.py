cff8e1e1634b701c9b088360fad1305c52ba8119
Felipe Lube de Bragança <felipe.lubra@gmail.com>
2019-02-27 15:58:35 -0300

    feat: Endpoints para a api para documentos e notícias

--- xram_memory/urls.py
+++ xram_memory/urls.py
@@ -4,6 +4,7 @@ from django.urls import include, path
 from django.conf.urls.static import static
 from django.conf import settings
 from xram_memory.artifact.admin.forms.news_bulk import news_bulk_insertion
+from xram_memory.artifact.views import DocumentViewSet, NewsViewSet
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -12,6 +13,14 @@ urlpatterns = [
 
 # TODO: proteger, somente usuários autenticados
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# Endpoints da API
+urlpatterns = [
+    path('api/v1/document/<int:pk>',
+         DocumentViewSet.as_view({'get': 'retrieve'})),
+    path('api/v1/news/<int:pk>',
+         NewsViewSet.as_view({'get': 'retrieve'}))
+] + urlpatterns
 
 
 if settings.DEBUG:
