3b1618c05ca07cfe805eea652e953b343fae6958
Felipe Lube de Bragança <felipe.lubra@gmail.com>
2019-03-01 12:32:40 -0300

    feat (api): adicione campos com as capturas de página (imagem e pdf)

--- xram_memory/artifact/serializers.py
+++ xram_memory/artifact/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework.serializers import ModelSerializer
-from xram_memory.artifact.models import Document, News, Newspaper
+from xram_memory.artifact.models import Document, News, Newspaper, NewsPDFCapture, NewsImageCapture
 from xram_memory.taxonomy.serializers import KeywordSerializer, SubjectSerializer
 
 
@@ -8,6 +8,20 @@ class DocumentSerializer(ModelSerializer):
         model = Document
         fields = ('id', 'title', 'teaser', 'slug',
                   'mime_type', 'file_size', 'file',)
+
+
+class PDFCaptureSerializer(ModelSerializer):
+    class Meta:
+        model = NewsPDFCapture
+        fields = ('pdf_document', 'pdf_capture_date',)
+    pdf_document = DocumentSerializer()
+
+
+class ImageCaptureSerializer(ModelSerializer):
+    class Meta:
+        model = NewsImageCapture
+        fields = ('image_document', 'image_document',)
+    image_document = DocumentSerializer()
 
 
 class NewspaperSerializer(ModelSerializer):
@@ -22,7 +36,9 @@ class NewsSerializer(ModelSerializer):
         model = News
         fields = ('id', 'title', 'teaser', 'slug',
                   'url', 'archived_news_url', 'authors', 'body', 'published_date', 'language', 'newspaper', 'keywords',
-                  'subjects',)
+                  'subjects', 'pdf_captures', 'image_capture')
     newspaper = NewspaperSerializer()
     keywords = KeywordSerializer(many=True)
     subjects = SubjectSerializer(many=True)
+    pdf_captures = PDFCaptureSerializer(many=True)
+    image_capture = ImageCaptureSerializer()
