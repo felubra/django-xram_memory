c954f4ad60f2bc90cb93745efb6def92410d4817
Felipe Lube de Bragança <felipe.lubra@gmail.com>
2019-03-03 16:30:38 -0300

    fix: defina as cores do editor numa variável para melhor legibilidade

--- xram_memory/page/admin.py
+++ xram_memory/page/admin.py
@@ -1,3 +1,5 @@
+import json
+
 from django import forms
 from django.contrib import admin
 from xram_memory.base_models import TraceableEditorialAdminModel
@@ -10,9 +12,24 @@ from .models import StaticPage
 
 class StaticPageAdminForm(forms.ModelForm):
     class Meta:
+        EDITOR_COLORS = json.dumps(["#e60000", "#ff9900", "#ffff00", "#008a00", "#0066cc",
+                                    "#9933ff", "#ffffff", "#facccc", "#ffebcc", "#ffffcc",
+                                    "#cce8cc", "#cce0f5", "#ebd6ff", "#bbbbbb", "#f06666",
+                                    "#ffc266", "#ffff66", "#66b966", "#66a3e0", "#c285ff",
+                                    "#888888", "#a10000", "#b26b00", "#b2b200", "#006100",
+                                    "#0047b2", "#6b24b2", "#444444", "#5c0000", "#663d00",
+                                    "#666600", "#003700", "#002966", "#3d1466", "#000000",
+                                    "#e60000", "#ff9900", "#ffff00", "#008a00", "#0066cc",
+                                    "#9933ff", "#facccc", "#ffebcc", "#ffffcc", "#cce8cc",
+                                    "#cce0f5", "#ebd6ff", "#bbbbbb", "#f06666", "#ffc266",
+                                    "#ffff66", "#66b966", "#66a3e0", "#c285ff", "#888888",
+                                    "#a10000", "#b26b00", "#b2b200", "#006100", "#0047b2",
+                                    "#6b24b2", "#444444", "#5c0000", "#663d00", "#666600",
+                                    "#003700", "#002966", "#3d1466"])
+
         widgets = {
             'body': QuillWidget(attrs={'placeholder': 'Conteúdo da página...',
-                                       'data-toolbar': '[[{"header":[2,3,4,5,6,false]}],["bold","italic","underline","strike"],[{"align":["","center","right","justify"]}],[{"color":["#e60000","#ff9900","#ffff00","#008a00","#0066cc","#9933ff","#ffffff","#facccc","#ffebcc","#ffffcc","#cce8cc","#cce0f5","#ebd6ff","#bbbbbb","#f06666","#ffc266","#ffff66","#66b966","#66a3e0","#c285ff","#888888","#a10000","#b26b00","#b2b200","#006100","#0047b2","#6b24b2","#444444","#5c0000","#663d00","#666600","#003700","#002966","#3d1466","#000000","#e60000","#ff9900","#ffff00","#008a00","#0066cc","#9933ff","#facccc","#ffebcc","#ffffcc","#cce8cc","#cce0f5","#ebd6ff","#bbbbbb","#f06666","#ffc266","#ffff66","#66b966","#66a3e0","#c285ff","#888888","#a10000","#b26b00","#b2b200","#006100","#0047b2","#6b24b2","#444444","#5c0000","#663d00","#666600","#003700","#002966","#3d1466"]},{"background":["#e60000","#ff9900","#ffff00","#008a00","#0066cc","#9933ff","#ffffff","#facccc","#ffebcc","#ffffcc","#cce8cc","#cce0f5","#ebd6ff","#bbbbbb","#f06666","#ffc266","#ffff66","#66b966","#66a3e0","#c285ff","#888888","#a10000","#b26b00","#b2b200","#006100","#0047b2","#6b24b2","#444444","#5c0000","#663d00","#666600","#003700","#002966","#3d1466","#000000","#e60000","#ff9900","#ffff00","#008a00","#0066cc","#9933ff","#facccc","#ffebcc","#ffffcc","#cce8cc","#cce0f5","#ebd6ff","#bbbbbb","#f06666","#ffc266","#ffff66","#66b966","#66a3e0","#c285ff","#888888","#a10000","#b26b00","#b2b200","#006100","#0047b2","#6b24b2","#444444","#5c0000","#663d00","#666600","#003700","#002966","#3d1466"]},"blockquote"],["link","image"],[{"list":"bullet"},{"list":"ordered"}],["clean"]]',
+                                       'data-toolbar': '[[{"header":[2,3,4,5,6,false]}],["bold","italic","underline","strike"],[{"align":["","center","right","justify"]}],[{"color":%s},{"background": %s},"blockquote"],["link","image"],[{"list":"bullet"},{"list":"ordered"}],["clean"]]' % (EDITOR_COLORS, EDITOR_COLORS),
                                        'data-formats': 'header,bold,italic,strike,underline,align,color,background,blockquote,link,list,image'}),
 
         }
