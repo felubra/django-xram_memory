48953b8e76839a0c2c6efd146c04728e5293da84
Felipe Lube de Bragança <felipe.lubra@gmail.com>
2019-03-03 15:52:17 -0300

    feat: endpoints da api para obter páginas e conjuntos de páginas comuns

---
+++ xram_memory/page/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework.serializers import ModelSerializer, CharField
+
+from .models import StaticPage
+
+
+class StaticPageSerializer(ModelSerializer):
+    """Um serializer com com todos os principais campos da página."""
+    class Meta:
+        model = StaticPage
+        fields = ('id', 'title', 'teaser', 'url',
+                  'body', 'image',
+                  'show_in_menu', 'published', 'featured', 'created_at', 'modified_at')
+
+
+class SimpleStaticPageSerializer(ModelSerializer):
+    """Um serializer com apenas alguns campos para salvar espaço."""
+    class Meta:
+        model = StaticPage
+        fields = ('id', 'title', 'teaser', 'url',
+                  'show_in_menu', 'published', 'featured',)
