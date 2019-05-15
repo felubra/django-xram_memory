861e9ddc573c0729ed802153da0f0bc7baea678d
Felipe Lube de Bragança <felipe.lubra@gmail.com>
2019-05-15 00:03:10 -0300

    refat: use uma variável com a url mais comum para teste

--- xram_memory/artifact/tests/lib/tests_news_fetcher.py
+++ xram_memory/artifact/tests/lib/tests_news_fetcher.py
@@ -27,29 +27,28 @@ class NewsFetcherTestCase(TestCase):
     def setUp(self):
         with open(str(Path(os.path.dirname(__file__), 'mocks', '0.html')), encoding='utf-8') as f:
             self.article_content = f.read()
+        self.defaultTestUrl = "https://politica.estadao.com.br/blogs/fausto-macedo/justica-decreta-bloqueio-de-r-5-bilhoes/"
 
     def test_fetch_basic_basic_fields(self):
         BASIC_FIELDS = ['title', 'image', 'body', 'teaser',
                         'published_date', 'authors', 'keywords', 'language']
 
         with requests_mock.Mocker() as m:
-        article_url = "https://politica.estadao.com.br/blogs/fausto-macedo/justica-decreta-bloqueio-de-r-5-bilhoes/"
-        m.register_uri('GET', article_url,
+            m.register_uri('GET', self.defaultTestUrl,
                        text=self.article_content)
 
         basic_info = NewsFetcher.fetch_basic_info(
-            article_url, fetch_images=False)
+                self.defaultTestUrl, fetch_images=False)
         for field in BASIC_FIELDS:
             self.assertIn(field, basic_info)
 
     def test_fetch_basic_basic_no_stopword_keyword(self):
-        article_url = "https://politica.estadao.com.br/blogs/fausto-macedo/justica-decreta-bloqueio-de-r-5-bilhoes/"
         with requests_mock.Mocker() as m:
-        m.register_uri('GET', article_url,
+            m.register_uri('GET', self.defaultTestUrl,
                        text=self.article_content)
 
         basic_info = NewsFetcher.fetch_basic_info(
-            article_url, fetch_images=False)
+                self.defaultTestUrl, fetch_images=False)
         self.assertEqual(basic_info["language"], "pt")
         for keyword in basic_info["keywords"]:
             self.assertNotIn(keyword, stopwords["pt"])
