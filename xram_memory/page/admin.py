b122115a6375b3937a3996a6470cc6bf87c7a8a5
Felipe Lube de Bragança <felipe.lubra@gmail.com>
2019-06-26 20:19:26 -0300

    refat: organize o código que gera um editor de texto rico

--- xram_memory/page/admin.py
+++ xram_memory/page/admin.py
@@ -2,51 +2,20 @@ import json
 
 from django import forms
 from django.contrib import admin
-from xram_memory.base_models import TraceableEditorialAdminModel
-from easy_thumbnails.widgets import ImageClearableFileInput
 from easy_thumbnails.fields import ThumbnailerImageField
-from xram_memory.quill_widget import QuillWidget
+from easy_thumbnails.widgets import ImageClearableFileInput
+from xram_memory.base_models import TraceableEditorialAdminModel
+from xram_memory.quill_widget import QuillWidget, make_editor_opt, TEASER_EDITOR_OPTIONS
 
 from .models import StaticPage
 
 
 class StaticPageAdminForm(forms.ModelForm):
     class Meta:
-        EDITOR_COLORS = json.dumps(["#e60000", "#ff9900", "#ffff00", "#008a00", "#0066cc",
-                                    "#9933ff", "#ffffff", "#facccc", "#ffebcc", "#ffffcc",
-                                    "#cce8cc", "#cce0f5", "#ebd6ff", "#bbbbbb", "#f06666",
-                                    "#ffc266", "#ffff66", "#66b966", "#66a3e0", "#c285ff",
-                                    "#888888", "#a10000", "#b26b00", "#b2b200", "#006100",
-                                    "#0047b2", "#6b24b2", "#444444", "#5c0000", "#663d00",
-                                    "#666600", "#003700", "#002966", "#3d1466", "#000000",
-                                    ])
-
-        BODY_EDITOR_OPTIONS = {
-            'placeholder': 'Conteúdo da página...',
-            'data-toolbar': '''[
-                                [{"header":[2,3,4,5,6,false]}],
-                                ["bold","italic","underline","strike"],
-                                [{"align":["","center","right","justify"]}],
-                                [{"color":%s},{"background": %s},"blockquote"],
-                                ["link","image"],[{"list":"bullet"},{"list":"ordered"}],
-                                ["clean"]]''' % (EDITOR_COLORS, EDITOR_COLORS),
-            'data-formats': '''header,bold,italic,strike,underline,align,color,
-                                                background,blockquote,link,list,image'''
-        }
-
-        TEASER_EDITOR_OPTIONS = {
-            'placeholder': 'Resumo ou chamada para esta página...',
-            'data-toolbar': '''[
-                                ["bold","italic","underline","strike"],
-                                [{"align":["","center","right","justify"]}],
-                                [{"color":%s},{"background": %s}],["link","image"],
-                                ["clean"]]''' % (EDITOR_COLORS, EDITOR_COLORS),
-            'data-formats': 'bold,italic,strike,underline,align,color,background,image,link'
-        }
-
         widgets = {
-            'body': QuillWidget(attrs=BODY_EDITOR_OPTIONS),
-            'teaser': QuillWidget(attrs=TEASER_EDITOR_OPTIONS),
+            'body': QuillWidget(attrs=make_editor_opt('Conteúdo da página...')),
+            'teaser': QuillWidget(attrs=make_editor_opt('Resumo ou chamada para esta página...',
+                                                        TEASER_EDITOR_OPTIONS)),
         }
 
 
--- xram_memory/quill_widget/__init__.py
+++ xram_memory/quill_widget/__init__.py
@@ -1,8 +1,44 @@
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext as _
 from django.forms.widgets import Widget
 from django.forms import Media
 from bs4 import BeautifulSoup
-from django.core.exceptions import ValidationError
-from django.utils.translation import gettext as _
+import json
+
+
+EDITOR_COLORS = json.dumps(["#e60000", "#ff9900", "#ffff00", "#008a00", "#0066cc",
+                            "#9933ff", "#ffffff", "#facccc", "#ffebcc", "#ffffcc",
+                            "#cce8cc", "#cce0f5", "#ebd6ff", "#bbbbbb", "#f06666",
+                            "#ffc266", "#ffff66", "#66b966", "#66a3e0", "#c285ff",
+                            "#888888", "#a10000", "#b26b00", "#b2b200", "#006100",
+                            "#0047b2", "#6b24b2", "#444444", "#5c0000", "#663d00",
+                            "#666600", "#003700", "#002966", "#3d1466", "#000000",
+                            ])
+
+DEFAULT_EDITOR = {
+    'data-toolbar': '''[
+                                [{"header":[2,3,4,5,6,false]}],
+                                ["bold","italic","underline","strike"],
+                                [{"align":["","center","right","justify"]}],
+                                [{"color":%s},{"background": %s},"blockquote"],
+                                ["link","image"],[{"list":"bullet"},{"list":"ordered"}],
+                                ["clean"]]''' % (EDITOR_COLORS, EDITOR_COLORS),
+    'data-formats': '''header,bold,italic,strike,underline,align,color,
+                                                background,blockquote,link,list,image'''
+}
+
+TEASER_EDITOR_OPTIONS = {
+    'data-toolbar': '''[
+                                ["bold","italic","underline","strike"],
+                                [{"align":["","center","right","justify"]}],
+                                [{"color":%s},{"background": %s}],["link","image"],
+                                ["clean"]]''' % (EDITOR_COLORS, EDITOR_COLORS),
+    'data-formats': 'bold,italic,strike,underline,align,color,background,image,link'
+}
+
+
+def make_editor_opt(placeholder, base_options=DEFAULT_EDITOR):
+    return {**base_options, **{'placeholder': placeholder}}
 
 
 def no_empty_html(value):
