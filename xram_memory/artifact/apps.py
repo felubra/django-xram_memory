46efaa84e3b0e21ebaf88efb84468229d65c77af
Felipe <felipe.lubra@gmail.com>
2020-04-08 21:09:27 -0300

    refat: remoção completa do código dos sinais no app Artifact

--- xram_memory/artifact/apps.py
+++ xram_memory/artifact/apps.py
@@ -4,6 +4,3 @@ from django.apps import AppConfig
 class ArtifactConfig(AppConfig):
     name = 'xram_memory.artifact'
     verbose_name = "Artefatos do acervo"
-
-    def ready(self):
-        from xram_memory.artifact import receivers
