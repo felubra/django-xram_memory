b0463d3db13e3dc27cef0472b6d7f6ad420206c8
Felipe <felipe.lubra@gmail.com>
2020-04-30 11:52:41 -0300

    feat: #112 parametrize a task com a configuração do app

--- xram_memory/lunr_index/tasks.py
+++ xram_memory/lunr_index/tasks.py
@@ -1,5 +1,19 @@
 from celery import shared_task, group
+from django.conf import settings
+from django.utils.timezone import now
+from datetime import timedelta
 
-@shared_task()
+SETTINGS = settings.LUNR_INDEX
+REBUILD_TIMEOUT = SETTINGS['REBUILD_TIMEOUT']
+REBUILD_INTERVAL = SETTINGS['REBUILD_INTERVAL']
+
+@shared_task(
+    retry_backoff=5,
+    max_retries=3,
+    retry_backoff_max=REBUILD_TIMEOUT / 4,
+    eta = now() + timedelta(seconds=REBUILD_INTERVAL),
+    retry_jitter=True,
+    time_limit=REBUILD_TIMEOUT
+)
 def lunr_index_rebuild():
     pass
