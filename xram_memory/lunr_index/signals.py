6069dbc79d20d7736bc26c2189256d0940fbe4ab
Felipe <felipe.lubra@gmail.com>
2020-05-09 18:44:42 -0300

    feat: #112 logging básico no sinal

--- xram_memory/lunr_index/signals.py
+++ xram_memory/lunr_index/signals.py
@@ -2,16 +2,17 @@ from django.db.models.signals import post_save, post_delete, m2m_changed
 from xram_memory.artifact.models import Document, News, Newspaper
 from xram_memory.utils import celery_is_avaliable, memcache_lock
 from xram_memory.taxonomy.models import Keyword, Subject
+from xram_memory.logger.decorators import log_process
 from datetime import datetime, timedelta
 from .tasks import lunr_index_rebuild
 from django.conf import settings
 from loguru import logger
-from celery.five import monotonic
 
 SETTINGS = settings.LUNR_INDEX
 REBUILD_INTERVAL = SETTINGS['REBUILD_INTERVAL']
 REBUILD_TIMEOUT = SETTINGS['REBUILD_TIMEOUT']
 
+@log_process(operation="agendar para reconstruir índice lunr")
 def schedule_lunr_index_rebuild(sender, instance, **kwargs):
     sync = not celery_is_avaliable()
     with memcache_lock('LUNR_INDEX_REBUILD', 'SIGNAL_SENDER', REBUILD_INTERVAL + REBUILD_TIMEOUT, sync) as (acquired, lock_info,):
