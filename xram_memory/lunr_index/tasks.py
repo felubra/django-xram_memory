e5997be73e14818f403e419e0dcc288e9df52fd4
Felipe <felipe.lubra@gmail.com>
2020-05-18 22:16:49 -0300

    refat: refatore lunr_index_rebuild - índice único para documentos e notícias - permita a geração de um índice vazio - adicione os campos newspaper, thumbnail, published_date

--- xram_memory/lunr_index/tasks.py
+++ xram_memory/lunr_index/tasks.py
@@ -1,4 +1,4 @@
-from xram_memory.utils import memcache_lock, release_memcache_lock
+from xram_memory.utils import memcache_lock, release_memcache_lock, datetime_to_string
 from xram_memory.lunr_index.util import LunrBackendValue
 from django.core.files.storage import get_storage_class
 from django.core.files.base import ContentFile
@@ -7,6 +7,7 @@ from django.utils.timezone import now
 from django.conf import settings
 from datetime import timedelta
 from django.apps import apps
+from itertools import chain
 from loguru import logger
 from lunr import lunr
 import requests
@@ -24,59 +25,81 @@ SEARCH_FIELDS = settings.LUNR_INDEX_SEARCH_FIELDS
 @shared_task(soft_time_limit=REBUILD_TIMEOUT)
 def lunr_index_rebuild(self, lock_info=None):
     # Construa um índice com documentos e notícias
-    # TODO: otimizar
     # TODO: tentar novamente em caso de erro
-    logger.debug("lunr_index_rebuild: início da execução")
-    News = apps.get_model('artifact', 'News')
-    Documents = apps.get_model('artifact', 'Document')
-
-    items = []
-    items += [
-        {
-            'id': n.id,
-            'title': n.title,
-            'teaser': n.teaser,
-            'uri': n.slug,
-            'subjects': " ".join([k.name for k in n.subjects.all()]),
-            'keywords': " ".join([k.name for k in n.keywords.all()]),
-        } for n in News.objects.prefetch_related('keywords', 'subjects')
-    ]
-    #FIXME: não repetir ids
-    items += [
-        {
-            'id': d.id,
-            'title': d.name,
-            'uri': d.document_id.hashid,
-            'teaser': d.description,
-            'subjects': " ".join([k.name for k in d.subjects.all()]),
-            'keywords': " ".join([k.name for k in d.keywords.all()]),
-        } for d in Documents.objects.prefetch_related('keywords', 'subjects')
-            .filter(document_id__isnull=False)
-            .filter(is_user_object=True)
-            .filter(is_public=True)
-    ]
     try:
-        if (len(items)):
-            if BACKEND == LunrBackendValue.BACKEND_REMOTE:
-                with requests.post(REMOTE_HOST, json={
-                    "documents": items,
-                    "config": {
-                        "searchFields": SEARCH_FIELDS,
-                        "saveDocument": SAVE_DOCUMENT
+        logger.debug("lunr_index_rebuild: início da execução")
+        News = apps.get_model('artifact', 'News')
+        Documents = apps.get_model('artifact', 'Document')
+
+        documents_to_index = []
+
+        news_queryset = News.objects.prefetch_related('keywords', 'subjects')
+        documents_queryset = (Documents.objects.prefetch_related('keywords', 'subjects')
+                .filter(document_id__isnull=False)
+                .filter(is_user_object=True)
+                .filter(is_public=True))
+
+        # Gere uma lista com as notícias e os documentos, com campos em comum
+        for idx, item in enumerate(chain(news_queryset, documents_queryset)):
+            try:
+                subjects = [k.name for k in item.subjects.all()]
+                keywords = [k.name for k in item.keywords.all()]
+
+                if BACKEND == LunrBackendValue.BACKEND_LOCAL:
+                    # o backend lunr.py não suporta lista como tipo de campo indexável
+                    subjects = " ".join(subjects)
+                    keywords = " ".join(keywords)
+
+                index_document = {
+                    'id': idx,
+                    'type': 'news' if isinstance(item, News) else 'document',
+                    'thumbnail': item.thumbnail,
+                    'subjects': subjects,
+                    'keywords': keywords,
+                }
+                if isinstance(item, News):
+                    index_document['published_date'] = datetime_to_string(item.published_date)
+                    index_document['title'] = item.title
+                    index_document['teaser'] = item.teaser
+                    index_document['uri'] = item.slug
+                    index_document['newspaper'] = {
+                        'title': item.newspaper.title,
+                        'favicon_logo': item.newspaper.favicon_logo,
+                        'url': item.newspaper.url,
                     }
-                }, headers={
-                    'Authorization': f'Bearer {REMOTE_SECRET}'
-                }) as r:
-                    r.raise_for_status()
-            else:
-                idx = lunr(
-                    ref='id',
-                    fields=[field for field in SEARCH_FIELDS if field != 'id'],
-                    documents=items, languages=['pt','en']
+                else:
+                    index_document['published_date'] = datetime_to_string(item.uploaded_at)
+                    index_document['newspaper'] = None
+                    index_document['title'] = item.name
+                    index_document['uri'] = item.document_id.hashid
+                    index_document['teaser'] = item.description
+                documents_to_index.append(index_document)
+            except Exception as e:
+                logger.debug("Falha ao indexar um(a) {}, com o id {}: {}"
+                    .format(item.verbose_name, item.pk, e)
                 )
-                serialized = idx.serialize()
-                storage = get_storage_class('xram_memory.utils.OverwriteDefaultStorage')()
-                path = storage.save(FILE_PATH, ContentFile(json.dumps(serialized)))
+
+
+        if BACKEND == LunrBackendValue.BACKEND_REMOTE:
+            with requests.post(REMOTE_HOST, json={
+                "documents": documents_to_index,
+                "config": {
+                    "searchFields": SEARCH_FIELDS,
+                    "saveDocument": SAVE_DOCUMENT
+                }
+            }, headers={
+                'Authorization': f'Bearer {REMOTE_SECRET}'
+            }) as r:
+                r.raise_for_status()
+        else:
+            idx = lunr(
+                ref='id',
+                fields=[field for field in SEARCH_FIELDS if field != 'id'],
+                documents=documents_to_index, languages=['pt','en']
+            )
+            serialized = idx.serialize()
+            storage = get_storage_class('xram_memory.utils.OverwriteDefaultStorage')()
+            path = storage.save(FILE_PATH, ContentFile(json.dumps(serialized)))
     finally:
         if lock_info:
             logger.debug("lunr_index_rebuild: limpando o lock")
