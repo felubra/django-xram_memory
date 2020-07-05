68f63f0e2e94e0b66e860b56c2e7ca54f54e779e
Felipe Lube de Bragança <felipe.lubra@gmail.com>
2020-07-05 14:49:08 -0300

    feat: testes

---
+++ tests/test_localsearch.py
@@ -0,0 +1,43 @@
+from tests.fixtures import NewsFactory
+from tests.utils import basic_news
+import requests_mock
+import tempfile
+import pytest
+import json
+import os
+from lunr.index import Index
+
+@pytest.mark.django_db(transaction=True)
+def test_localsearch_remote_elastic_lunr(settings):
+    settings.LUNR_INDEX_BACKEND = 'remote'
+    settings.LUNR_INDEX_REMOTE_HOST = 'http://localhost:5000'
+    settings.LUNR_INDEX_REMOTE_SECRET = 'alabamba'
+
+    with requests_mock.Mocker() as m:
+        m.register_uri('POST', settings.LUNR_INDEX_REMOTE_HOST, text='success')
+        with basic_news() as news:
+            news.save()
+        # verifique se a requisição foi chamada
+        assert m.called
+        # verifique se foi enviada uma requisição para o servidor configurado
+        reqs = [r for r in m.request_history if settings.LUNR_INDEX_REMOTE_HOST in r.url]
+        assert len(reqs)
+
+@pytest.mark.django_db(transaction=True)
+def test_localsearch_lunr_py(settings):
+    settings.LUNR_INDEX_BACKEND = 'local'
+
+    try:
+        fd, file_path, = tempfile.mkstemp(dir=settings.MEDIA_ROOT)
+        settings.LUNR_INDEX_FILE_PATH = file_path
+        with basic_news() as news:
+            news.save()
+            with open(file_path, 'r') as json_index:
+                data = json_index.read()
+                idx = Index.load(data)
+                assert len(idx.search(news.title))
+    finally:
+        try:
+            os.close(fd)
+        finally:
+            os.remove(file_path)
