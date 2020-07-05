c028d988b6b7b0007b9d3146c1cbedf69c4505e7
Felipe Lube de Bragança <felipe.lubra@gmail.com>
2020-07-05 15:01:07 -0300

    fix: lunr_index_rebuild: sempre verifique o valor das configurações em runtime

--- xram_memory/lunr_index/tasks.py
+++ xram_memory/lunr_index/tasks.py
@@ -10,25 +10,21 @@ from xram_memory.lunr_index.lib.index_builders import (
     build_with_remote_elastic_lunr
 )
 
-BACKEND = settings.LUNR_INDEX_BACKEND
-FILE_PATH = settings.LUNR_INDEX_FILE_PATH
-REBUILD_INTERVAL = settings.LUNR_INDEX_REBUILD_INTERVAL
-REBUILD_TIMEOUT = settings.LUNR_INDEX_REBUILD_TIMEOUT
-REMOTE_HOST = settings.LUNR_INDEX_REMOTE_HOST
-REMOTE_SECRET = settings.LUNR_INDEX_REMOTE_SECRET
-SAVE_DOCUMENT = settings.LUNR_INDEX_SAVE_DOCUMENT
-SEARCH_FIELDS = settings.LUNR_INDEX_SEARCH_FIELDS
-
-@shared_task(soft_time_limit=REBUILD_TIMEOUT)
+@shared_task(soft_time_limit=settings.LUNR_INDEX_REBUILD_TIMEOUT)
 def lunr_index_rebuild(self, lock_info=None):
     # Construa um índice com documentos e notícias
     # TODO: tentar novamente em caso de erro
     try:
         logger.debug("lunr_index_rebuild: início da execução")
-        if BACKEND == LunrBackendValue.BACKEND_REMOTE:
-            build_with_remote_elastic_lunr(REMOTE_HOST, REMOTE_SECRET, SEARCH_FIELDS, SAVE_DOCUMENT)
-        else:
-            build_with_lunr_py(FILE_PATH)
+        if settings.LUNR_INDEX_BACKEND == LunrBackendValue.BACKEND_REMOTE:
+            build_with_remote_elastic_lunr(
+                settings.LUNR_INDEX_REMOTE_HOST,
+                settings.LUNR_INDEX_REMOTE_SECRET,
+                settings.LUNR_INDEX_SEARCH_FIELDS,
+                settings.LUNR_INDEX_SAVE_DOCUMENT
+            )
+        elif settings.LUNR_INDEX_BACKEND == LunrBackendValue.BACKEND_LOCAL:
+            build_with_lunr_py(settings.LUNR_INDEX_FILE_PATH)
     finally:
         if lock_info:
             logger.debug("lunr_index_rebuild: limpando o lock")
