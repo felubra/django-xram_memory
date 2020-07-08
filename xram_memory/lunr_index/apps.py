cfbc8790e7890d928e813b122dfdb8dab8f30613
Felipe Lube de Bragança <felipe.lubra@gmail.com>
2020-07-07 23:52:23 -0300

    feat: processador de sinais desligável em lunr_search - encapsule o funcionamento dos sinais num objeto, de forma a permitir a conexão/desconexão dos sinais em runtime, o que será usado nos testes

--- xram_memory/lunr_index/apps.py
+++ xram_memory/lunr_index/apps.py
@@ -1,8 +1,15 @@
 from django.apps import AppConfig
-
+from .util import LunrBackendValue
 
 class LunrIndexConfig(AppConfig):
     name = 'xram_memory.lunr_index'
 
     def ready(self):
-        from xram_memory.lunr_index import signals
+        # importe aqui, pois precisamos esperar as outras aplicações iniciar
+        from .signals import SignalProcessor
+        from django.conf import settings
+        # Somente conecte os sinais se o backend for válido
+        if getattr(settings, 'LUNR_INDEX_BACKEND', None) in LunrBackendValue.VALID_BACKENDS:
+            REBUILD_INTERVAL = settings.LUNR_INDEX_REBUILD_INTERVAL
+            REBUILD_TIMEOUT = settings.LUNR_INDEX_REBUILD_TIMEOUT
+            self.signal_processor = SignalProcessor(REBUILD_INTERVAL, REBUILD_TIMEOUT)
--- xram_memory/lunr_index/signals.py
+++ xram_memory/lunr_index/signals.py
@@ -9,25 +9,45 @@ from django.conf import settings
 from loguru import logger
 from xram_memory.lunr_index.util import LunrBackendValue
 
-REBUILD_INTERVAL = settings.LUNR_INDEX_REBUILD_INTERVAL
-REBUILD_TIMEOUT = settings.LUNR_INDEX_REBUILD_TIMEOUT
 
-@log_process(operation="agendar para reconstruir índice lunr")
-def schedule_lunr_index_rebuild(sender, instance, **kwargs):
-    sync = not celery_is_avaliable()
-    with memcache_lock('LUNR_INDEX_REBUILD', 'SIGNAL_SENDER', REBUILD_INTERVAL + REBUILD_TIMEOUT, sync) as (acquired, lock_info,):
-        if acquired:
-            logger.debug("schedule_lunr_index_rebuild: lock adquirido")
-            if not sync:
-                lunr_index_rebuild.apply_async(eta=datetime.utcnow() + timedelta(seconds=REBUILD_INTERVAL), args=[lock_info])
-            else:
-                lunr_index_rebuild.apply(args=[lock_info])
-    # FIXME: implementar failback para o caso do celery não estar disponível
+class SignalProcessor:
+    def __init__(self, rebuild_interval, rebuild_timeout):
+        self.rebuild_interval = rebuild_interval
+        self.rebuild_timeout = rebuild_timeout
+        #TODO: permitir configuração
+        self.models = [News, Newspaper, Keyword, Subject, Document]
+        self.setup()
+
+    def setup(self):
+        for model in self.models:
+            post_save.connect(self.schedule_lunr_index_rebuild, model)
+            m2m_changed.connect(self.schedule_lunr_index_rebuild, model)
+            post_delete.connect(self.schedule_lunr_index_rebuild, model)
+
+    def teardown(self):
+        for model in self.models:
+            post_save.disconnect(self.schedule_lunr_index_rebuild, model)
+            m2m_changed.disconnect(self.schedule_lunr_index_rebuild, model)
+            post_delete.disconnect(self.schedule_lunr_index_rebuild, model)
+
+    @log_process(operation="agendar para reconstruir índice lunr")
+    def schedule_lunr_index_rebuild(self, sender, instance, **kwargs):
+        # FIXME: implementar failback para o caso do celery não estar disponível
+        sync = not celery_is_avaliable()
+        with memcache_lock('LUNR_INDEX_REBUILD', 'SIGNAL_SENDER', REBUILD_INTERVAL + REBUILD_TIMEOUT, sync) as (acquired, lock_info,):
+            if acquired:
+                logger.debug("schedule_lunr_index_rebuild: lock adquirido")
+                if not sync:
+                    lunr_index_rebuild.apply_async(eta=datetime.utcnow() + timedelta(seconds=REBUILD_INTERVAL), args=[lock_info])
+                else:
+                    lunr_index_rebuild.apply(args=[lock_info])
 
 
-# Somente conecte os sinais se o backend for válido, útil para testes
-if getattr(settings, 'LUNR_INDEX_BACKEND', None) in LunrBackendValue.VALID_BACKENDS:
-    for Model in [News, Newspaper, Keyword, Subject, Document]:
-        post_save.connect(schedule_lunr_index_rebuild, Model)
-        m2m_changed.connect(schedule_lunr_index_rebuild, Model)
-        post_delete.connect(schedule_lunr_index_rebuild, Model)
+
+
+
+
+
+
+
+
