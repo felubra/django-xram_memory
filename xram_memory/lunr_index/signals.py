9b0a3c8c58e34e450459fa4f20d670b5a63b4303
Felipe Lube de Bragança <felipe.lubra@gmail.com>
2020-07-08 00:41:53 -0300

    fix: correção em schedule_lunr_index_rebuild; documentação

--- xram_memory/lunr_index/signals.py
+++ xram_memory/lunr_index/signals.py
@@ -5,12 +5,14 @@ from xram_memory.taxonomy.models import Keyword, Subject
 from xram_memory.logger.decorators import log_process
 from datetime import datetime, timedelta
 from .tasks import lunr_index_rebuild
-from django.conf import settings
 from loguru import logger
-from xram_memory.lunr_index.util import LunrBackendValue
+
 
 
 class SignalProcessor:
+    """ Observa os modelos registrados através de seus sinais e agenda o trabalho de indexação.
+    Pode ser desligado via runtime.
+    """
     def __init__(self, rebuild_interval, rebuild_timeout):
         self.rebuild_interval = rebuild_interval
         self.rebuild_timeout = rebuild_timeout
@@ -19,26 +21,40 @@ class SignalProcessor:
         self.setup()
 
     def setup(self):
+        """
+        Para cada modelo, conecta o agendamento do trabalho ao sinal suportado.
+        """
         for model in self.models:
             post_save.connect(self.schedule_lunr_index_rebuild, model)
             m2m_changed.connect(self.schedule_lunr_index_rebuild, model)
             post_delete.connect(self.schedule_lunr_index_rebuild, model)
 
     def teardown(self):
+        """
+        Desconecta os sinais outrora conectados.
+        """
         for model in self.models:
             post_save.disconnect(self.schedule_lunr_index_rebuild, model)
             m2m_changed.disconnect(self.schedule_lunr_index_rebuild, model)
             post_delete.disconnect(self.schedule_lunr_index_rebuild, model)
 
     @log_process(operation="agendar para reconstruir índice lunr")
-    def schedule_lunr_index_rebuild(self, sender, instance, **kwargs):
-        # FIXME: implementar failback para o caso do celery não estar disponível
+    def schedule_lunr_index_rebuild(self, **kwargs):
+        """
+        Com base nas configurações, obtém uma trava e agenda a execução das funções de indexação
+        """
         sync = not celery_is_avaliable()
-        with memcache_lock('LUNR_INDEX_REBUILD', 'SIGNAL_SENDER', REBUILD_INTERVAL + REBUILD_TIMEOUT, sync) as (acquired, lock_info,):
+        with memcache_lock(
+                'LUNR_INDEX_REBUILD', 'SIGNAL_SENDER',
+                self.rebuild_interval + self.rebuild_timeout, sync
+            ) as (acquired, lock_info,):
             if acquired:
                 logger.debug("schedule_lunr_index_rebuild: lock adquirido")
                 if not sync:
-                    lunr_index_rebuild.apply_async(eta=datetime.utcnow() + timedelta(seconds=REBUILD_INTERVAL), args=[lock_info])
+                    lunr_index_rebuild.apply_async(
+                        eta=datetime.utcnow() + timedelta(seconds=self.rebuild_interval),
+                        args=[lock_info]
+                    )
                 else:
                     lunr_index_rebuild.apply(args=[lock_info])
 
