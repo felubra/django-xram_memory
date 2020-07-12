459bd75419444257f03e4552b38ed06931ee0b9b
Felipe Lube de Bragança <felipe.lubra@gmail.com>
2020-07-11 22:36:13 -0300

    refat: utilize xram_memory.utils.classes.SignalProcessor

--- xram_memory/lunr_index/apps.py
+++ xram_memory/lunr_index/apps.py
@@ -6,10 +6,10 @@ class LunrIndexConfig(AppConfig):
 
     def ready(self):
         # importe aqui, pois precisamos esperar as outras aplicações iniciar
-        from .signals import SignalProcessor
+        from .signals import LunrIndexSignalProcessor
         from django.conf import settings
         # Somente conecte os sinais se o backend for válido
         if getattr(settings, 'LUNR_INDEX_BACKEND', None) in LunrBackendValue.VALID_BACKENDS:
             REBUILD_INTERVAL = settings.LUNR_INDEX_REBUILD_INTERVAL
             REBUILD_TIMEOUT = settings.LUNR_INDEX_REBUILD_TIMEOUT
-            self.signal_processor = SignalProcessor(REBUILD_INTERVAL, REBUILD_TIMEOUT)
+            self.signal_processor = LunrIndexSignalProcessor(REBUILD_INTERVAL, REBUILD_TIMEOUT)
--- xram_memory/lunr_index/signals.py
+++ xram_memory/lunr_index/signals.py
@@ -2,6 +2,7 @@ from django.db.models.signals import post_save, post_delete, m2m_changed
 from xram_memory.artifact.models import Document, News, Newspaper
 from xram_memory.utils import celery_is_avaliable, memcache_lock
 from xram_memory.utils.decorators import disable_for_loaddata
+from xram_memory.utils.classes import SignalProcessor
 from xram_memory.taxonomy.models import Keyword, Subject
 from xram_memory.logger.decorators import log_process
 from datetime import datetime, timedelta
@@ -10,38 +11,21 @@ from loguru import logger
 
 
 
-class SignalProcessor:
+
+class LunrIndexSignalProcessor(SignalProcessor):
     """ Observa os modelos registrados através de seus sinais e agenda o trabalho de indexação.
     Pode ser desligado via runtime.
     """
     def __init__(self, rebuild_interval, rebuild_timeout):
         self.rebuild_interval = rebuild_interval
         self.rebuild_timeout = rebuild_timeout
-        #TODO: permitir configuração
         self.models = [News, Newspaper, Keyword, Subject, Document]
-        self.setup()
-
-    def setup(self):
-        """
-        Para cada modelo, conecta o agendamento do trabalho ao sinal suportado.
-        """
-        for model in self.models:
-            post_save.connect(self.schedule_lunr_index_rebuild, model)
-            m2m_changed.connect(self.schedule_lunr_index_rebuild, model)
-            post_delete.connect(self.schedule_lunr_index_rebuild, model)
-
-    def teardown(self):
-        """
-        Desconecta os sinais outrora conectados.
-        """
-        for model in self.models:
-            post_save.disconnect(self.schedule_lunr_index_rebuild, model)
-            m2m_changed.disconnect(self.schedule_lunr_index_rebuild, model)
-            post_delete.disconnect(self.schedule_lunr_index_rebuild, model)
+        self.signals = [post_save, post_delete, m2m_changed]
+        super().__init__()
 
     @log_process(operation="agendar para reconstruir índice lunr")
     @disable_for_loaddata
-    def schedule_lunr_index_rebuild(self, **kwargs):
+    def handler(self, *arsg, **kwargs):
         """
         Com base nas configurações, obtém uma trava e agenda a execução das funções de indexação
         """
