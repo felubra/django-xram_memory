7e8d9c77bb13d60ab0521b579db4ca33a672944d
Felipe Lube de Bragança <felipe.lubra@gmail.com>
2020-07-11 22:35:22 -0300

    feat: funcionalidade de um processador de sinais plugável

---
+++ xram_memory/utils/classes.py
@@ -0,0 +1,54 @@
+'''
+Define classes utilizadas em todo o projeto
+'''
+from abc import ABC, abstractmethod
+
+class SignalProcessorBase(ABC):
+    signals = []
+    models = []
+
+    @abstractmethod
+    def setup(self):
+        """
+        Para cada modelo, conecta o agendamento do trabalho ao sinal suportado.
+        """
+        pass
+
+    @abstractmethod
+    def teardown(self):
+        """
+        Desconecta os sinais outrora conectados.
+        """
+        pass
+
+    @abstractmethod
+    def handler(self, *args, **kwargs):
+        """
+        A função a ser executada, nunca deve ser chamado diretamente
+        """
+        pass
+
+class SignalProcessor(SignalProcessorBase):
+    '''
+    Um processador de sinais que os associa a modelos definidos e pode
+    ser ligado/desligado em runtime
+    '''
+    def __init__(self):
+        super().__init__()
+        self.setup()
+
+    def setup(self):
+        """
+        Para cada modelo, conecta o agendamento do trabalho ao sinal suportado.
+        """
+        for signal in self.signals:
+            for model in self.models:
+                signal.connect(self.handler, model)
+
+    def teardown(self):
+        """
+        Desconecta os sinais outrora conectados.
+        """
+        for signal in self.signals:
+            for model in self.models:
+                signal.disconnect(self.handler, model)
