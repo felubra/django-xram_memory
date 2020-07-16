38e32afd91d14848b2e0baa32874520a53b64ee9
Felipe Lube de Bragança <felipe.lubra@gmail.com>
2020-07-16 00:11:10 -0300

    feat(dev): permita debugar app e testes remotamente

--- conftest.py
+++ conftest.py
@@ -1,3 +1,8 @@
 import sys
+import os
+if os.environ.get('REMOTE_TESTING'):
+    import ptvsd
+    ptvsd.enable_attach(address=("0.0.0.0", 5678))
+    ptvsd.wait_for_attach()
 
 collect_ignore_glob = ['.venv/*']
--- manage.py
+++ manage.py
@@ -9,6 +9,13 @@ if __name__ == '__main__':
 
     try:
         from configurations.management import execute_from_command_line
+        from django.conf import settings
+
+        if settings.DEBUG:
+            if os.environ.get('RUN_MAIN') or os.environ.get('WERKZEUG_RUN_MAIN'):
+                import ptvsd
+                ptvsd.enable_attach(address = ('0.0.0.0', 3000))
+                print("Attached remote debugger")
     except ImportError as exc:
         raise ImportError(
             'Couldn\'t import Django. Are you sure it\'s installed and '
