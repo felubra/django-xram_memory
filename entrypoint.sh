ba988e4380db564b1ea9a7345dc42514c0da8a24
Felipe Lube de Bragança <felipe.lubra@gmail.com>
2020-07-16 07:44:22 -0300

    feat(dev): atualização do script entrypoint - permita a execução do servidor de desenvolvimento do Django

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 set -e;
-python manage.py collectstatic --noinput --noinput -i jquery-ui*
-python manage.py migrate                  # Aplique as migrações de banco de dados
+python manage.py collectstatic --noinput --noinput -i jquery-ui* # Colecione arquivos estáticos
+python manage.py migrate # Aplique as migrações de banco de dados
 
-# Inicie os processos do gunicorn
-echo Starting Gunicorn.
-exec gunicorn xram_memory.wsgi \
-    --name xram_memory_webadmin \
-    --bind 0.0.0.0:8000 \
-    --timeout 90 \
-    --workers 3 \
-    --log-level=info \
-    "$@"
+environment="${ENVIRONMENT:-production}"
+echo $environment
+if [ "$environment" == "Development"  ]; then
+    # Inicie o servidor de desenvolvimento do Django
+    echo Starting Django Development Server.
+    exec ./manage.py runserver_plus 0.0.0.0:8000
+else
+    # Inicie os processos do gunicorn
+    echo Starting Gunicorn.
+    exec gunicorn xram_memory.wsgi \
+        --name xram_memory_webadmin \
+        --bind 0.0.0.0:8000 \
+        --timeout 90 \
+        --workers 3 \
+        --log-level=info \
+        "$@"
+fi
