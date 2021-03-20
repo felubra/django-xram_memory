4c7dc99beab4ff1e1689bf6dbbfc6a8c52c62ecc
Felipe Lube de Bragança <felipe.lubra@gmail.com>
2021-03-19 23:21:08 -0300

    feat: allow download corpora to user folder

--- scripts/download_corpora.py
+++ scripts/download_corpora.py
@@ -1,9 +1,11 @@
-# -*- coding: utf-8 -*-
+#!/usr/bin/python3
 """
 Downloads the necessary NLTK models and corpora required to support
 all of newspaper's features. Modify for your own needs.
 """
+import sys
 import nltk
+import argparse
 
 REQUIRED_CORPORA = [
     'brown',  # Required for FastNPExtractor
@@ -15,12 +17,23 @@ REQUIRED_CORPORA = [
 ]
 
 
-def main():
+def main(argv):
+    parser = argparse.ArgumentParser(description='Downloads and installs nltk corpora.')
+    parser.add_argument('--location', '-l',
+        default="system",
+        choices=["user", "system"],
+        dest='location'
+    )
+    location = getattr(parser.parse_args(), 'location')
+
     for each in REQUIRED_CORPORA:
         print(('Downloading "{0}"'.format(each)))
-        nltk.download(each, download_dir='/usr/share/nltk_data')
+        if location == 'system':
+            nltk.download(each, download_dir='/usr/share/nltk_data')
+        else:
+            nltk.download(each)
     print("Finished.")
 
 
 if __name__ == '__main__':
-    main()
+   main(sys.argv[1:])
